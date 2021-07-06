0c7c79c38e1938188e8f681371597be9c36f7c84
atsu8864 <atsuya8864@i.softbank.jp>
2021-07-07 00:17:16 +0900

    Initial Commit

---
+++ apple_tutorial/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  apple_tutorial
+//
+//  Created by 宮矢 on 2021/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ apple_tutorial/apple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_tutorialApp.swift
+//  apple_tutorial
+//
+//  Created by 宮矢 on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct apple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
